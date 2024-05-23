685e07fc70822722ce6aac66021e5794e757f517
mlp <118296511+eutectic6337@users.noreply.github.com>
2024-05-23 19:20:39 +1000

    expanded brightness to Red,Green,Blue

--- software/prototype-1/badge_smartled.h
+++ software/prototype-1/badge_smartled.h
@@ -47,18 +47,20 @@ struct color_sequence {
   struct color_transition* tx;
   unsigned n;
 }
-struct color_step {
-  Time next;
-  signed delta;
-  Time delta_t;
-  Time started;
-  struct color_sequence* tx;
-};
 struct color_loop {
   unsigned slow_down;
   unsigned speed_up;
   struct color_sequence* s[];
 }
+struct color_step {
+  Time next;
+  signed delta;
+  Time delta_t;
+  Time started;
+  struct color_transition* tx;
+  struct color_sequence* seq;
+  struct color_loop* loop;
+};
 
 struct brightness_transition {
   Time ms;
@@ -70,18 +72,20 @@ struct brightness_sequence {
   struct brightness_transition* tx;
   unsigned n;
 }
-struct brightness_step {
-  Time next;
-  signed delta;
-  Time delta_t;
-  Time started;
-  struct brightness_sequence* tx;
-};
 struct brightness_loop {
   unsigned slow_down;
   unsigned speed_up;
   struct brightness_sequence* s[];
 }
+struct brightness_step {
+  Time next;
+  signed delta;
+  Time delta_t;
+  Time started;
+  struct brightness_transition* tx;
+  struct brightness_sequence* seq;
+  struct brightness_loop* loop;
+};
 
 const struct brightness_transition flash_99pc[] = {
   {0,-1}, {990,-1},
@@ -120,11 +124,13 @@ const struct color_transition rainbow[] = {
 struct LED_state {
   struct color_loop cloop;
   CRGB color;
-  struct color_step Rstep, Gstep, Bstep;
+  struct color_step Rstep;
+  struct color_step Gstep;
+  struct color_step Bstep;
 
   struct brightness_loop bloop;
   unsigned brightness;
-  struct brightness_step bstep;
+  struct brightness_step bright_step;
 } city[NUM_SMART_LEDS];
 
 int any_LED_changed = 0;
@@ -133,47 +139,158 @@ void update_city(int i) {
   static Time log_delay;
   // ======== CUSTOMIZE HERE ========
 
+  Time now = millis();
 
-  if (millis() > s.next) {
-    v += s.delta; // and MAKE IT SO
-    s.next += s.delta_t;
+  //Brightness
+  if (now >= city[i].bright_step.next) {
+    any_LED_changed = 1;
+    city[i].brightness += city[i].bright_step.delta; // and MAKE IT SO
+    city[i].bright_step.next += city[i].bright_step.delta_t;
   }
-  if (millis() > s.started + s.tx->ms) {
-    s.started = millis();
-    struct transition* n = s.tx->next;
+  if (now > city[i].bright_step.started + city[i].bright_step.tx->ms) {
+    city[i].bright_step.started = now;
+    struct brightness_transition* n = city[i].bright_step.tx->next;
     if (!n) { ?? }
     if (!n->ms) { ?? }
     signed d = n->target - v;
     if (d == 0) {
-      s.delta = 0;
-      s.delta_t = n->ms;
+      city[i].bright_step.delta = 0;
+      city[i].bright_step.delta_t = n->ms;
     }
     else {
       value abs_d = abs(d);
       if (abs_d < n->ms) {
         if (d < 0) {
-          s.delta = -1;
+          city[i].bright_step.delta = -1;
         }
         else if (d > 0) {
-          s.delta = 1;
+          city[i].bright_step.delta = 1;
         }
-        s.delta_t = n->ms / abs_d;
+        city[i].bright_step.delta_t = n->ms / abs_d;
       }
       else {
-        s.delta_t = 1;
-        s.delta = d / n->ms;
+        city[i].bright_step.delta_t = 1;
+        city[i].bright_step.delta = d / n->ms;
       }
     }
-    s.tx = n;
-    s.next = s.started + s.delta;
+    city[i].bright_step.tx = n;
+    city[i].bright_step.next = city[i].bright_step.started + city[i].bright_step.delta;
+  }
+
+  //Red
+  if (now >= city[i].Rstep.next) {
+    any_LED_changed = 1;
+    city[i].color += CRGB(city[i].Rstep.delta, 0, 0); // and MAKE IT SO
+    city[i].Rstep.next += city[i].Rstep.delta_t;
+  }
+  if (now > city[i].Rstep.started + city[i].Rstep.tx->ms) {
+    city[i].Rstep.started = now;
+    struct brightness_transition* n = city[i].Rstep.tx->next;
+    if (!n) { ?? }
+    if (!n->ms) { ?? }
+    signed d = n->target - v;
+    if (d == 0) {
+      city[i].Rstep.delta = 0;
+      city[i].Rstep.delta_t = n->ms;
+    }
+    else {
+      value abs_d = abs(d);
+      if (abs_d < n->ms) {
+        if (d < 0) {
+          city[i].Rstep.delta = -1;
+        }
+        else if (d > 0) {
+          city[i].Rstep.delta = 1;
+        }
+        city[i].Rstep.delta_t = n->ms / abs_d;
+      }
+      else {
+        city[i].Rstep.delta_t = 1;
+        city[i].Rstep.delta = d / n->ms;
+      }
+    }
+    city[i].Rstep.tx = n;
+    city[i].Rstep.next = city[i].Rstep.started + city[i].Rstep.delta;
+  }
+
+  //Green
+  if (now >= city[i].Gstep.next) {
+    any_LED_changed = 1;
+    city[i].color += CRGB(0, city[i].Gstep.delta, 0); // and MAKE IT SO
+    city[i].Gstep.next += city[i].Gstep.delta_t;
+  }
+  if (now > city[i].Gstep.started + city[i].Gstep.tx->ms) {
+    city[i].Gstep.started = now;
+    struct brightness_transition* n = city[i].Gstep.tx->next;
+    if (!n) { ?? }
+    if (!n->ms) { ?? }
+    signed d = n->target - v;
+    if (d == 0) {
+      city[i].Gstep.delta = 0;
+      city[i].Gstep.delta_t = n->ms;
+    }
+    else {
+      value abs_d = abs(d);
+      if (abs_d < n->ms) {
+        if (d < 0) {
+          city[i].Gstep.delta = -1;
+        }
+        else if (d > 0) {
+          city[i].Gstep.delta = 1;
+        }
+        city[i].Gstep.delta_t = n->ms / abs_d;
+      }
+      else {
+        city[i].Gstep.delta_t = 1;
+        city[i].Gstep.delta = d / n->ms;
+      }
+    }
+    city[i].Gstep.tx = n;
+    city[i].Gstep.next = city[i].Gstep.started + city[i].Gstep.delta;
+  }
+
+  //Blue
+  if (now >= city[i].Bstep.next) {
+    any_LED_changed = 1;
+    city[i].color += CRGB(0, 0, city[i].Bstep.delta); // and MAKE IT SO
+    city[i].Bstep.next += city[i].Bstep.delta_t;
+  }
+  if (now > city[i].Bstep.started + city[i].Bstep.tx->ms) {
+    city[i].Bstep.started = now;
+    struct brightness_transition* n = city[i].Bstep.tx->next;
+    if (!n) { ?? }
+    if (!n->ms) { ?? }
+    signed d = n->target - v;
+    if (d == 0) {
+      city[i].Bstep.delta = 0;
+      city[i].Bstep.delta_t = n->ms;
+    }
+    else {
+      value abs_d = abs(d);
+      if (abs_d < n->ms) {
+        if (d < 0) {
+          city[i].Bstep.delta = -1;
+        }
+        else if (d > 0) {
+          city[i].Bstep.delta = 1;
+        }
+        city[i].Bstep.delta_t = n->ms / abs_d;
+      }
+      else {
+        city[i].Bstep.delta_t = 1;
+        city[i].Bstep.delta = d / n->ms;
+      }
+    }
+    city[i].Bstep.tx = n;
+    city[i].Bstep.next = city[i].Bstep.started + city[i].Bstep.delta;
   }
 
 
 
-  if (millis() >= city[i].wait_until) {
-    any_LED_changed = 1;
     city[i].wait_until = millis() + city[i].time_step;
     leds[i] = leds[i].lerp8(city[i].new_LED, city[i].step);
+
+
     if (city[i].step == 255) {
       /* set new transition for this city */
 
